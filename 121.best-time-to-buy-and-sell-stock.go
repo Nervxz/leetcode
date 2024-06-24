14200f2ebee9ffb1f668f2849fa6db51b6e83662
Thai Nguyen Khang <127386953+Nervxz@users.noreply.github.com>
2024-06-24 22:16:06 +0700

    Create 121.best-time-to-buy-and-sell-stock.go

---
+++ 121.best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,59 @@
+package main
+
+// Best Time to Buy and Sell Stock
+//
+// Easy
+//
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+//
+// You are given an array `prices` where `prices[i]` is the price of a given
+// stock on the `ith` day.
+//
+// You want to maximize your profit by choosing a **single day** to buy one
+// stock and choosing a **different day in the future** to sell that stock.
+//
+// Return _the maximum profit you can achieve from this transaction_. If you
+// cannot achieve any profit, return `0`.
+//
+// **Example 1:**
+//
+// ```
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
+// 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you
+// must buy before you sell.
+//
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+//
+// ```
+//
+// **Constraints:**
+//
+// - `1 <= prices.length <= 105`
+// - `0 <= prices[i] <= 104`
+
+func maxProfit(prices []int) int {
+	profit := 0
+	buy := prices[0]
+	// Iterate through the prices array starting from the second element
+	for _, sell := range prices[1:] {
+		// If the current sell price is higher than the buy price
+		if sell > buy {
+			// Update the maximum profit if the current profit is higher
+			profit = max(profit, sell-buy)
+		} else {
+			// Update the buy price if the current sell price is lower
+			buy = sell
+		}
+	}
+	return profit
+}
